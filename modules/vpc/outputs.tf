a6aee525dc706acda6dc863392a30ae37e4963cd
smathankar <smathankar01@gmail.com>
2025-07-04 02:16:25 +0530

    Delete modules directory

--- modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  value       = aws_vpc.main.id
-}
-output "subnet_1_id" {
-  value       = aws_subnet.public_subnet.*.id[0]
-}
-output "subnet_2_id" {
-  value       = aws_subnet.public_subnet.*.id[1]
-}
--- modules/vpc/variables.tf
+++
@@ -1,2 +0,0 @@
-variable "vpc_cidr" { type = string }
-variable "prefix" { type = string }
