78f2a524079afe2372e1dfd54e562dd4fb44f665
smathankar <smathankar01@gmail.com>
2025-07-04 02:05:05 +0530

    Create outputs.tf

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  value       = aws_vpc.main.id
+}
+output "subnet_1_id" {
+  value       = aws_subnet.public_subnet.*.id[0]
+}
+output "subnet_2_id" {
+  value       = aws_subnet.public_subnet.*.id[1]
+}
