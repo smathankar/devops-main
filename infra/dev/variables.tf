6cc6ef66ad6ca9dea9af386626d8fd1c87ea95fa
smathankar <smathankar01@gmail.com>
2025-07-04 02:51:42 +0530

    Update variables.tf

--- infra/dev/variables.tf
+++ infra/dev/variables.tf
@@ -2,6 +2,7 @@
 # VPC
 variable "vpc_cidr" { type = string }
 variable "prefix" { type = string }
+variable "aws_region" { type = string }
 
 
 
@@ -9,17 +10,6 @@ variable "prefix" { type = string }
 
 
 
-
-
-
-
-
-
-#variable "aws_region" {
-#  description = "AWS region to deploy resources in"
-#  type        = string
-#  default     = "us-east-1"
-#}
 
 #variable "bucket_name" {
 #  description = "Unique name for the S3 bucket"
