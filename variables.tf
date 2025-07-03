9fde632c7860acd27bb83ada1ad4db3776e06bf8
smathankar <smathankar01@gmail.com>
2025-07-04 01:28:53 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Unique name for the S3 bucket"
+  type        = string
+  default     = "my-unique-bucket-6765757343" # Replace with a unique name
+}
