5da1e829230d7426e4924dbcc3e549eecedc699d
smathankar <smathankar01@gmail.com>
2025-07-04 01:49:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
-  tags = {
-    Name        = var.bucket_name
-    Environment = "test1"
-    Project     = "Terraform"
-    Owner       = "DevOpsTeam"
-  }
-}
+#resource "aws_s3_bucket" "my_bucket" {
+#  bucket = var.bucket_name
+#  acl    = "private"
+#  tags = {
+#    Name        = var.bucket_name
+#    Environment = "test1"
+#    Project     = "Terraform"
+#    Owner       = "DevOpsTeam"
+#  }
+#}
