1d64d79091fa3fe83f42882d8feac3427a58b6d9
smathankar <smathankar01@gmail.com>
2025-07-04 08:28:55 +0530

    Update eks.tf

--- infra/modules/eks/eks.tf
+++ infra/modules/eks/eks.tf
@@ -1,1 +1,14 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.prefix
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
+  vpc_config {
+    subnet_ids = var.subnet_ids
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSVPCResourceController
+  ]
+}
