fd8b347f9fea41ec8945647238a4878707f76c59
smathankar <smathankar01@gmail.com>
2025-07-04 10:42:28 +0530

    Update ecr.tf

--- infra/modules/eks/ecr.tf
+++ infra/modules/eks/ecr.tf
@@ -3,3 +3,9 @@ resource "aws_ecr_repository" "ecr_repo" {
   #image_tag_mutability = "MUTABLE"
 
 }
+
+resource "aws_ssm_parameter" "store_ecr_name" {
+  name  = "${var.prefix}-store-ecr-name"
+  type  = "String"
+  value = aws_ecr_repository.ecr_repo.name
+}
