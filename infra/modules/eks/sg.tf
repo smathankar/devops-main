368f45fffeb1957fcf3fe176e72cf96be09089d9
smathankar <smathankar01@gmail.com>
2025-07-04 08:25:09 +0530

    Create sg.tf

---
+++ infra/modules/eks/sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "eks_cluster_sg" {
+  name   = "${var.prefix}-cluster-sg"
+  vpc_id = aws_vpc.eks_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.prefix}-cluster-sg"
+  }
+}
+
+#################################################################
+
