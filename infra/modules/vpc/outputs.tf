3b6fa15c33230e9095be2c3bc1f61d62ada7b709
smathankar <smathankar01@gmail.com>
2025-07-04 08:40:30 +0530

    Update outputs.tf

--- infra/modules/vpc/outputs.tf
+++ infra/modules/vpc/outputs.tf
@@ -1,9 +1,15 @@
 output "vpc_id" {
   value       = aws_vpc.main.id
 }
-output "subnet_1_id" {
-  value       = aws_subnet.public_subnet.*.id[0]
+#output "subnet_1_id" {
+#  value       = aws_subnet.public_subnet.*.id[0]
+#}
+#output "subnet_2_id" {
+#  value       = aws_subnet.public_subnet.*.id[1]
+#}
+output "subnet_ids" {
+  value       = aws_subnet.public_subnet.*.id
 }
-output "subnet_2_id" {
-  value       = aws_subnet.public_subnet.*.id[1]
-}
+
+
+
